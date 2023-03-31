c7dfd5e168ae5ed02492462e7d8e9599afbd6ed4
leonardopach <pachecodossantos17@gmail.com>
2023-03-31 16:21:20 -0300

    javascript

--- fundamentos/objeto2.js
+++ fundamentos/objeto2.js
@@ -1,10 +1,28 @@
 console.log(typeof Object)
 console.log(typeof new Object)
 
-const Cliente = function() {}
+const Cliente = function () { }
 console.log(typeof Cliente)
 console.log(typeof new Cliente)
 
-class Produto {} // ES 2015 (ES6)
+class Produto { } // ES 2015 (ES6)
 console.log(typeof Produto)
 console.log(typeof new Produto())
+
+
+function criarPessoa(nome, sobrenome, idade) {
+    return {
+        nome,
+        sobrenome,
+        idade,
+
+        falar() {
+            console.log("Ola mundo");
+        }
+    };
+}
+
+const pessoa1 = criarPessoa("leonardo", "pacheco", 25);
+
+pessoa1.falar();
+console.log(pessoa1);
---
+++ fundamentos/trocavalor.js
@@ -0,0 +1,8 @@
+let valor1 = 10;
+let valor2 = 20;
+let valor3 = 40;
+[valor1, valor2, valor3] = [valor3, valor2, valor1];
+
+console.log(valor1);
+console.log(valor2);
+console.log(valor3);
