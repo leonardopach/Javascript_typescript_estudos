72a472a944132dbf64881ed40315fe14aa2cbe92
leonardopach <leonardopach@LeonardoPach>
2023-03-31 20:52:49 -0300

    javascrip

---
+++ operadores_logicos/curto_circuito.js
@@ -0,0 +1,8 @@
+function falaOi() {
+    return 'Oi'
+}
+
+const vaiExecutar = false;
+
+console.log(vaiExecutar && falaOi());
+console.log(vaiExecutar || falaOi()); 
---
+++ operadores_logicos/operador_logico.js
@@ -0,0 +1,7 @@
+const expressaoAnd = true && true && true && false;
+const expressaoOr = true || false;
+const negacao = !true;
+
+console.log(expressaoAnd);
+console.log(expressaoOr);
+console.log(negacao);
